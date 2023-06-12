6315d42e829618f6e6682e9e0fe27ab94efccc22
patricio.stickar <patricio.stickar@moveax.it>
2023-06-12 17:04:55 +0200

    first commit

---
+++ add_directory_to_direnv_whitelist.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Get the absolute path of the current directory
+current_dir="$(pwd)"
+
+# Set the path to the direnv.toml file
+toml_file="${HOME}/.config/direnv/direnv.toml"
+
+# Create the direnv directory if it doesn't exist
+mkdir -p "$(dirname "$toml_file")"
+
+# Check if the file exists
+if [ -f "$toml_file" ]; then
+  # Check if the prefix is already present
+  if grep -q "prefix = \[" "$toml_file"; then
+    # Check if the current directory is already in the list
+    if grep -q "\"$current_dir\"" "$toml_file"; then
+      echo "Directory already in the prefix list."
+      exit 0
+    fi
+
+    # Append the new prefix to the existing list
+    sed -i "s|\(prefix = \[.*\)\]|\\1, \"$current_dir\"\\]|" "$toml_file"
+  else
+    # Add the new prefix section
+    echo "prefix = [ \"$current_dir\" ]" >> "$toml_file"
+  fi
+else
+  # Create the file with the new prefix
+  echo "[whitelist]" >> "$toml_file"
+  echo "prefix = [ \"$current_dir\" ]" >> "$toml_file"
+fi
+
+echo "direnv.toml file updated at ${toml_file}"
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(process.version);
+console.log("IDE variable: " + process.env.NAME);
+console.log(".env variable: " + process.env.STATUS);
