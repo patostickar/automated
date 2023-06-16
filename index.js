0a0ddb745ed01ee83a51ebdca0c9d8cbbc90f373
patricio.stickar <patricio.stickar@moveax.it>
2023-06-16 16:43:33 +0200

    remove direnv saved variables

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log(process.version);
 console.log("IDE variable: " + process.env.NAME);
 console.log(".env variable: " + process.env.STATUS);
+console.log("1Password variable" + process.env.SECRET);
